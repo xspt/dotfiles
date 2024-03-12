5e78a82b5f548b78a3911d6a4b0016d2053c637d
xspt <82736724+xspt@users.noreply.github.com>
2024-03-12 09:51:44 -0300

    Add nvim-tree relative number

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,4 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+   view = {
+      number = true,
+      relativenumber = true,
+   },
+})
