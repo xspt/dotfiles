8421c7524c5942b96df221f8310f9daee92e1947
xspt <82736724+xspt@users.noreply.github.com>
2024-06-23 11:34:45 -0300

    Add vimtex to neovim plugins

--- nvim/lua/xspt/plugins.lua
+++ nvim/lua/xspt/plugins.lua
@@ -35,6 +35,15 @@ local plugins = {
       'nvim-lualine/lualine.nvim',
       dependencies = { 'nvim-tree/nvim-web-devicons' },
    },
+   {
+      "lervag/vimtex",
+      lazy = false,     -- we don't want to lazy load VimTeX
+      -- tag = "v2.15", -- uncomment to pin to a specific release
+      init = function()
+         -- VimTeX configuration goes here, e.g.
+         vim.g.vimtex_view_method = "zathura"
+      end
+   },
    -- LSP
    'neovim/nvim-lspconfig',
    'williamboman/mason.nvim',
