7a810db1c89a804d44cce97f8113352aa75ededa
mthsgimenez <82736724+mthsgimenez@users.noreply.github.com>
2026-07-13 08:55:36 -0300

    fix: escaping waybar

--- waybar/playerctl.sh
+++ waybar/playerctl.sh
@@ -3,18 +3,16 @@
 PREV_OUTPUT=""
 
 while true; do
-        if MUSIC=$(playerctl -s -p $MUSIC_PLAYER metadata -f "{{trunc(artist, 20)}} - {{trunc(title, 30)}}"); then
-                if [[ $(playerctl -s -p $MUSIC_PLAYER status) == "Paused" ]]; then
+        if MUSIC=$(playerctl -s -p "$MUSIC_PLAYER" metadata -f "{{trunc(artist, 20)}} - {{trunc(title, 30)}}"); then
+                if [[ $(playerctl -s -p "$MUSIC_PLAYER" status) == "Paused" ]]; then
                         MUSIC="*Paused*"
-                elif [[ "$MUSIC" == *"&"* ]]; then
-                        MUSIC="${MUSIC//&/&amp;}"
                 fi
         else
                 MUSIC="No music playing"
         fi
 
-        if [ "$PREV_OUTPUT" != "$MUSIC" ]; then
-                echo $MUSIC
+        if [[ "$MUSIC" != "$PREV_OUTPUT" ]]; then
+                echo "$MUSIC"
                 PREV_OUTPUT="$MUSIC"
         fi
 
